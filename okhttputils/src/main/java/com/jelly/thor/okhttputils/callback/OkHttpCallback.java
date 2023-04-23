ed4b6f9044aa34bdb980e60c4b1c8f2101578e4f
wudongdong <withwudongdong@gmail.com>
2023-04-23 17:06:36 +0800

    「feature」升级agp  8.0 「feature」修改okHttpCallback注解

--- okhttputils/src/main/java/com/jelly/thor/okhttputils/callback/OkHttpCallback.java
+++ okhttputils/src/main/java/com/jelly/thor/okhttputils/callback/OkHttpCallback.java
@@ -10,7 +10,7 @@ import java.lang.reflect.Type;
 import okhttp3.Response;
 
 /**
- * 类描述：当前项目数据初步解析 fastJson写法<br/>
+ * 类描述：当前项目数据初步解析 Gson写法<br/>
  * 创建人：吴冬冬<br/>
  * 创建时间：2022/4/27 11:25 <br/>
  */
